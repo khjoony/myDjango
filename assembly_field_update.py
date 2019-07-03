f7d6c06579f78b3ef935dbb95efad9273e605b3a
khjoony <khjoony4u@gmail.com>
2019-07-04 01:32:22 +0900

    Print assembly_list

--- assembly_field_update.py
+++ assembly_field_update.py
@@ -9,10 +9,14 @@ django.setup()
 from assembly.models import Member
 
 Q_SET = Member.objects.all()
-## print(amK.MEMBER_IDS, amK.KR_NAMES, amC.CHI_NAMES, amE.EN_NAMES)
+for i in range(0, len(amK.KR_NAMES)):
+    print(amK.MEMBER_IDS[i], amK.KR_NAMES[i], amC.CHI_NAMES[i], amE.EN_NAMES[i])
+## Update KR_Names
+"""
 _ID = 1
 for chi_name in amC.CHI_NAMES:
     member_instance = Member.objects.get(id=_ID)
     member_instance.chi_name = chi_name
     member_instance.save()
     _ID += 1
+"""
